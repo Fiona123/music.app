6b785e10aa294525b2eb485b1afc00643f592330
Li, Fiona <fiona.li@sap.com>
2018-04-11 17:11:57 +0800

    create slider component to display recommend pictures

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,28 +1,44 @@
 <template>
-	<div>I am recommend</div>
+	<div>
+		<slider v-if="recommends.length">
+			<div v-for="(item,index) in recommends" :key="index">
+				<a :href="item.linkUrl">
+					<img :src="item.picUrl">
+				</a>
+			</div>
+		</slider>
+	</div>
 </template>
 
 <script type="text/ecmascript-6">
 	import {getRecommend} from 'api/recommend'
 	import {ERR_OK} from 'api/config'
+	import Slider from 'base/slider'
 
 	export default {
 		created () {
 			this._getRecommend()
+		},
+		data () {
+			return {
+				recommends: []
+			}
 		},
 		methods: {
 			_getRecommend () {
 				getRecommend().then((res) => {
 					if (res.code === ERR_OK) {
 						console.log(res.data.slider)
-						console.log('i am testing')
+						this.recommends = res.data.slider
 					}
 				})
 			}
+		},
+		components: {
+			Slider
 		}
 	}
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
 </style>
