6455bf4c8a8480a53fbc69a28112bd65ac5daca1
Li, Fiona <fiona.li@sap.com>
2018-04-11 13:51:11 +0800

    initialize vue router and create tab component for router

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
 	<MHeader></MHeader>
+	<tab></tab>
+	<router-view></router-view>
   </div>
 </template>
 
 <script>
 
 import MHeader from 'components/m-header/m-header'
+import Tab from 'components/tab/tab'
 
 export default {
   name: 'App',
   components: {
-	MHeader
+	MHeader,
+	Tab
   }
 }
 </script>
