3cb5178454fcf856b25e97a0323b1b3e1b7e5611
李芳芳 <fangfangli@emotibot.com>
2018-08-31 12:19:53 +0800

    add player.vue

--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -46,10 +46,8 @@ export function prefixStyle(style) {
 	if (vendor === false) {
 		return false
 	}
-
 	if (vendor === 'standard') {
 		return style
 	}
-
 	return vendor + style.charAt(0).toUpperCase() + style.substr(1)
 }
--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -1,66 +1,96 @@
 <template>
     <div class="player" v-show="playList.length > 0">
-        <div class="normal-player" v-show="fullScreen">
-            <div class="background">
-                <img width="100%" height="100%" :src="currentSong.image">
-            </div>
-            <div class="top">
-                <div class="back" @click="back">
-                    <i class="icon-back"></i>
+        <transition name="normal"
+        @enter="enter"
+        @after-enter="afterEnter"
+        @leave="leave"
+        @after-leave="afterLeave">
+            <div class="normal-player" v-show="fullScreen">
+                <div class="background">
+                    <img width="100%" height="100%" :src="currentSong.image">
                 </div>
-                <h1 class="title" v-html="currentSong.name"></h1>
-                <h2 class="subtitle" v-html="currentSong.singer"></h2>
-            </div>
-            <div class="middle">
-                <div class="middle-l" ref="middleL">
-                    <div class="cd-wrapper" ref="cdWrapper">
-                        <div class="cd" ref="imageWrapper">
-                            <img ref="image" class="image" :class="cdCls" :src="currentSong.image">
+                <div class="top">
+                    <div class="back" @click="back">
+                        <i class="icon-back"></i>
+                    </div>
+                    <h1 class="title" v-html="currentSong.name"></h1>
+                    <h2 class="subtitle" v-html="currentSong.singer"></h2>
+                </div>
+                <div class="middle">
+                    <div class="middle-l" ref="middleL">
+                        <div class="cd-wrapper" ref="cdWrapper">
+                            <div class="cd" ref="imageWrapper">
+                                <img ref="image" class="image" :class="cdCls" :src="currentSong.image">
+                            </div>
+                        </div>
+                        <div class="playing-lyric-wrapper">
+                            <div class="playing-lyric">{{playingLyric}}</div>
                         </div>
                     </div>
-                    <div class="playing-lyric-wrapper">
-                        <div class="playing-lyric">{{playingLyric}}</div>
-                    </div>
+                    <scroll class="middle-r" ref="lyricList" :data="currentLyric && currentLyric.lines">歌词显示</scroll>
                 </div>
-                <scroll class="middle-r" ref="lyricList" :data="currentLyric && currentLyric.lines">歌词显示</scroll>
-            </div>
-            <div class="bottom">
-                <div class="dot-wrapper">
-                    <span class="dot" :class="{'active': currentShow === 'cd'}"></span>
-                    <span class="dot" :class="{'active': currentShow === 'lyric'}"></span>
-                </div>
-                <div class="progress-wrapper">
-                    <span class="time time-l">0:21</span>
-                    <div class="progress-bar-wrapper">
-                        progress bar
+                <div class="bottom">
+                    <div class="dot-wrapper">
+                        <span class="dot" :class="{'active': currentShow === 'cd'}"></span>
+                        <span class="dot" :class="{'active': currentShow === 'lyric'}"></span>
                     </div>
-                    <span class="time time-r">4:35</span>
-                </div>
-                <div class="operators">
-                    <div class="icon i-left">
-                        <i :class="iconMode"></i>
+                    <div class="progress-wrapper">
+                        <span class="time time-l">0:21</span>
+                        <div class="progress-bar-wrapper">
+                            progress bar
+                        </div>
+                        <span class="time time-r">4:35</span>
                     </div>
-                    <div class="icon i-left" :class="disableCls">
-                        <i class="icon-prev"></i>
-                    </div>
-                    <div class="icon i-center" :class="disableCls">
-                        <i class="needsclick" :class="playIcon"></i>
-                    </div>
-                    <div class="icon i-right" :class="disableCls">
-                        <i class="icon-next"></i>
-                    </div>
-                    <div class="icon i-right">
-                        <i class="icon" :class="favoriteIcon"></i>
+                    <div class="operators">
+                        <div class="icon i-left">
+                            <i :class="iconMode"></i>
+                        </div>
+                        <div class="icon i-left" :class="disableCls">
+                            <i class="icon-prev"></i>
+                        </div>
+                        <div class="icon i-center" :class="disableCls">
+                            <i class="needsclick" :class="playIcon"></i>
+                        </div>
+                        <div class="icon i-right" :class="disableCls">
+                            <i class="icon-next"></i>
+                        </div>
+                        <div class="icon i-right">
+                            <i class="icon" :class="favoriteIcon"></i>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="mini-player" v-show="!fullScreen" @click="open"></div>
+        </transition>
+        <transition name="mini">
+            <div class="mini-player" v-show="!fullScreen" @click="open">
+                <div class="icon">
+                    <div class="imgWrapper" ref="miniWrapper">
+                        <img :src="currentSong.image" width="40" height="40" :class="cdCls">
+                    </div>
+                </div>
+                <div class="text">
+                    <h2 class="name" v-html="currentSong.name"></h2>
+                    <p class="desc" v-html="currentSong.singer"></p>
+                </div>
+                <div class="control">
+                    con
+                </div>
+                <div class="control" @click.stop="showPlaylist">
+                    <i class="icon-playlist"></i>
+                </div>
+            </div>
+        </transition>
+        <audio ref="audio" :src="currentSong.url"></audio>
     </div>
 </template>
 
 <script>
 import {mapGetters, mapMutations} from 'vuex'
+import animations from 'create-keyframe-animation'
+import {prefixStyle} from 'common/js/dom'
+
+const transform = prefixStyle('transform')
+// const transitionDuration = prefixStyle('transitionDuration')
 
 export default {
     data() {
@@ -71,6 +101,7 @@ export default {
         }
     },
     computed: {
+        // 播放或暂停
         cdCls() {
             return 'play'
             // return this.playing ? 'play' : ''
@@ -87,12 +118,82 @@ export default {
             'currentSong'
         ])
     },
+    watch: {
+        currentSong() {
+            this.$nextTick(() => {
+                this.$refs.audio.play()
+            })
+        }
+    },
     methods: {
         back() {
             this.setFullScreen(false)
         },
         open() {
             this.setFullScreen(true)
+        },
+        enter(el, done) {
+            // 定义动画
+            const {x, y, scale} = this._getPosAndScale()
+            let animation = {
+                0: {
+                    transform: `translate3d(${x}px,${y}px,0) scale(${scale})`
+                },
+                60: {
+                    transform: `translate3d(0,0,0) scale(1.1)`
+                },
+                100: {
+                    transform: `translate3d(0,0,0) scale(1)`
+                }
+            }
+            // 注册动画
+            animations.registerAnimation({
+                name: 'move',
+                animation,
+                presets: {
+                    duration: 400,
+                    easing: 'linear'
+                }
+            })
+            // 在Element上运行动画，运行结束后调用done回调
+            animations.runAnimation(this.$refs.cdWrapper, 'move', done)
+        },
+        afterEnter() {
+            // 清空animation
+            animations.unregisterAnimation('move')
+            this.$refs.cdWrapper.style.animation = ''
+        },
+        leave(el, done) {
+            this.$refs.cdWrapper.style.transition = 'all 0.4s'
+            const {x, y, scale} = this._getPosAndScale()
+            this.$refs.cdWrapper.style[transform] = `translate3d(${x}px,${y}px,0) scale(${scale})`
+            const timer = setTimeout(done, 400)
+            this.$refs.cdWrapper.addEventListener('transitionend', () => {
+                clearTimeout(timer)
+                done()
+            })
+        },
+        afterLeave() {
+            this.$refs.cdWrapper.style.transition = ''
+            this.$refs.cdWrapper.style[transform] = ''
+        },
+        _getPosAndScale() {
+            // 左下角的位置
+            const targetWidth = 40
+            const paddingLeft = 40
+            const paddingBottom = 30
+            // 大图的位置
+            const paddingTop = 80
+            const width = window.innerWidth * 0.8
+            // 缩放比例
+            const scale = targetWidth / width
+            const x = -(window.innerWidth / 2 - paddingLeft)
+            const y = window.innerHeight - paddingTop - width / 2 - paddingBottom
+            return {
+                x,
+                y,
+                scale
+            }
         },
         ...mapMutations({
             setFullScreen: 'SET_FULL_SCREEN'
@@ -254,6 +355,16 @@ export default {
                     text-align: left
                 .icon-favorite
                     color: $color-sub-theme
+        &.normal-enter-active, &.normal-leave-active
+            transition: all 0.4s
+            .top, .bottom
+                transition all 0.4s cubic-bezier(0.86, 0.18, 0.82, 1.32)
+        &.normal-enter, &.normal-leave-to
+            opacity: 0
+            .top
+                transform: translate3d(0, -100px, 0)
+            .bottom
+                transform: translate3d(0, 100px, 0)
     .mini-player
         position: fixed;
         left: 0;
@@ -262,4 +373,50 @@ export default {
         height: 60px;
         background: $color-highlight-background;
         z-index: 180;
+        display: flex;
+        align-items: center;
+        .icon
+            width: 40px;
+            height: 40px;
+            padding: 0 10px 0 20px;
+            .imgWrapper
+                height: 100%;
+                width: 100%;
+                img
+                    border-radius: 50%;
+                    &.play
+                        animation: rotate 10s linear infinite
+                    &.icon-pause
+                        animation-play-state: paused
+        .text
+            flex: 1;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            line-height: 20px;
+            overflow: hidden;
+            .name
+                margin-bottom: 2px;
+                no-wrap()
+                font-size: $font-size-medium
+                color: $color-text
+            .desc
+                no-wrap()
+                font-size: $font-size-small
+                color: $color-text-d
+        .control
+            width: 30px;
+            padding: 0 10px;
+            .icon-playlist, .icon-play-mini, .icon-pause-mini
+                font-size: 30px;
+                color: $color-theme-d
+        &.mini-enter-active, &.mini-leave-active
+            transition: all 0.4s
+        &.mini-enter, &.mini-leave-active
+            opacity: 0
+    @keyframes rotate
+        0%
+            transform: rotate(0)
+        100%
+            transform: rotate(360deg)
 </style>
